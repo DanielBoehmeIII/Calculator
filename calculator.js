fae79cbf9cdc5f6b3a500abd6832c91e11bb0fed
DanielBoehmeIII <172210340+DanielBoehmeIII@users.noreply.github.com>
2024-12-27 07:20:02 -0500

    Implement main logic and front-end design for calc

---
+++ calculator.js
@@ -0,0 +1,22 @@
+document
+  .querySelector(".calculator")
+  .addEventListener("click", function (event) {
+    const button = event.target;
+    const display = document.getElementById("display");
+
+    if (button.classList.contains("display-button")) {
+      display.value += button.value;
+    } else if (button.id.includes("clear-button")) {
+      display.value = "";
+    } else if (button.id.includes("delete-button")) {
+      display.value = display.value.toString().slice(0, -1);
+    } else {
+      display.value = eval(display.value);
+    }
+
+    button.style.backgroundColor = "#09acff";
+
+    setTimeout(() => {
+      button.style.backgroundColor = "";
+    }, 30);
+  });
