1ef6f17b3659ebc6a65a7e0af2b2eaf753036d25
DanielBoehmeIII <172210340+DanielBoehmeIII@users.noreply.github.com>
2024-12-28 07:46:29 -0500

    Implement scrolling and refactor eval()
    - Add scrolling to far right using .scrollWidth
    - refactor eval() with array and string methods

--- calculator.js
+++ calculator.js
@@ -9,12 +9,46 @@ document
 
     if (button.classList.contains("display-button")) {
       display.value += button.value;
+      display.scrollLeft = display.scrollWidth;
     } else if (button.id.includes("clear-button")) {
       display.value = "";
     } else if (button.id.includes("delete-button")) {
       display.value = display.value.toString().slice(0, -1);
-    } else {
-      display.value = eval(display.value);
+      display.scrollLeft = display.scrollWidth;
+    } else if (button.id.includes("evaluate-button")) {
+      const expressionArray = display.value.toString().split(/([+\-*/])/);
+      for (let i = 0; i < expressionArray.length; i++) {
+        if (expressionArray[i] === "*") {
+          expressionArray[i - 1] =
+            parseFloat(expressionArray[i - 1]) *
+            parseFloat(expressionArray[i + 1]);
+          expressionArray.splice(i, 2);
+          i--;
+        } else if (expressionArray[i] === "/") {
+          expressionArray[i - 1] =
+            parseFloat(expressionArray[i - 1]) /
+            parseFloat(expressionArray[i + 1]);
+          expressionArray.splice(i, 2);
+          i--;
+        }
+      }
+      for (let i = 0; i < expressionArray.length; i++) {
+        if (expressionArray[i] === "+") {
+          expressionArray[i - 1] =
+            parseFloat(expressionArray[i - 1]) +
+            parseFloat(expressionArray[i + 1]);
+          expressionArray.splice(i, 2);
+          i--;
+        } else if (expressionArray[i] === "-") {
+          expressionArray[i - 1] =
+            parseFloat(expressionArray[i - 1]) -
+            parseFloat(expressionArray[i + 1]);
+          expressionArray.splice(i, 2);
+          i--;
+        }
+      }
+      display.value = expressionArray[0];
+      display.scrollLeft = display.scrollWidth;
     }
 
     button.style.backgroundColor = "#09acff";
